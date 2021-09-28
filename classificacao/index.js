eeda60f9087445f9721cf68410f16a4ed9717820
famoraes13 <62120531+famoraes13@users.noreply.github.com>
2021-09-27 21:30:46 -0300

    Aula 27/09 - aplicação da classificação e consultas

---
+++ classificacao/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+
+const palavraChave = "importante";
+
+const funcoes = {
+    ObservacaoCriada: (observacao) =>{
+        observacao.status = observacao.texto.includes(palavraChave) ?
+        "importante":
+        "comum";
+        axios.post('http://localhost:10000', {
+        tipo:"ObservaçãoClassificada",
+        dados: observacao,
+    });
+
+    }
+}
+app.post('/eventos', (req,res) =>{
+try{
+    funcoes[req.body.tipo](req.body.dados);
+}catch(err){}
+res.status(200).send({msg: "ok"})
+
+});
+
+app.listen(7000,() => {
+    console.log('Classificacao Porta 7000')
+});
